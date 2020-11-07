09e8aa8aa6fef3c85f5a0b09810d790120bdf3f7
jarparrish <jarparrish@gmail.com>
2020-11-06 19:32:51 -0700

    ex-3 finished and passing test

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,26 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    integer_dictionary = {}
+    result = []
+    num_lists = len(arrays)
+    for int_list in arrays:
+        
+        for list_int in int_list:
+            if list_int in integer_dictionary:
+                integer_dictionary[list_int] += 1
+            else:
+                integer_dictionary[list_int] = 1
+
+    
+    sorted_integer_dictionary_list = list(sorted(integer_dictionary.items(), key=lambda x: x[1], reverse=True))
+
+    for item in sorted_integer_dictionary_list:
+
+        if item[1] == num_lists:
+            result.append(item[0])
+        else:
+            break
 
     return result
 
@@ -15,3 +35,4 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
